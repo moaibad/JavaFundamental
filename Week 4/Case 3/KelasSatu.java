931cdadb2c077d20a6bc2095576074425fbe42bc
manto <95458318+moaibad@users.noreply.github.com>
2022-02-27 00:35:42 +0700

    Create KelasSatu.java

---
+++ Week 4/Case 3/KelasSatu.java
@@ -0,0 +1,19 @@
+package kelas;
+
+public class KelasSatu {
+    {
+        System.out.println(11); /*ditampilkan ketika constructor KelasSatu dipanggil*/
+    }
+    
+    static{
+        System.out.println(2); /*hanya ditampilkan satu kali di awal ketika constructor dipanggil*/
+    }
+    
+    public KelasSatu(int i){
+        System.out.println(3); /*ditampilkan ketika constructor KelasSatu dengan parameter dipanggil*/
+    }
+    
+    public KelasSatu(){
+        System.out.println(4); /*ditampilkan ketika constructor KelasSatu tanpa parameter dipanggil*/
+    }
+}
