ad72c3ce372e0db9891a01e837413c0895355982
mantoZONE <fathulibad.412@gmail.com>
2022-04-03 01:44:06 +0700

    add multiple inheritance task

---
+++ Week 8/Exercise 3/Sortable.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author Moaibad
+ */
+interface Sortable {
+    public abstract int compare(Sortable b);
+    public static void shell_sort(Sortable[] a){     
+        //Shell sort body
+        
+        /*For loop sampai interval tidak dibawah 0*/
+        for(int interval = a.length/2; interval>0 ; interval/=2){
+            
+            /*For loop membandingkan dua buah elemen berdasarkan interval*/
+            for(int i = interval; i<a.length; i++){
+                int j = i;
+                
+                /*Proses swap apabila elemen tidak sort by ascending dengan method compare*/
+                while(j>= interval && a[j].compare(a[j-interval]) < 0){
+                    Sortable temp = a[j];
+                    a[j] = a[j-interval];
+                    a[j-interval] = temp;
+                    j = j-interval;
+                }
+            }
+        }
+    }
+}
