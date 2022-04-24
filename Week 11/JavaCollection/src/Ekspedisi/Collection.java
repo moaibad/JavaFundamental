ccb53a73c3c0e61ea0eb4f83ef0b2fed18b9a626
manto <95458318+moaibad@users.noreply.github.com>
2022-04-24 23:26:34 +0700

    Update Collection.java

--- Week 11/JavaCollection/src/Ekspedisi/Collection.java
+++ Week 11/JavaCollection/src/Ekspedisi/Collection.java
@@ -1,5 +1,6 @@
 package Ekspedisi;
 
+import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
@@ -8,7 +9,7 @@ public class Collection {
 
 	@SerializedName("ekspedisi")
 	@Expose
-	private List<Ekspedisi> ekspedisi = null;
+	private List<Ekspedisi> ekspedisi = new ArrayList<Ekspedisi>();
 
 	public List<Ekspedisi> getEkspedisi() {
 		return ekspedisi;
